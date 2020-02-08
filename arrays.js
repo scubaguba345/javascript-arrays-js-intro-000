18050da2fe14bf1d056837ced26a5b49ca283aeb
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-02-08 18:44:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var candyString = "Yes"
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
